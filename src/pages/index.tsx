edf9c54872a6584932baa4129c67d559f7830c4f
CDE90 <ethan@ethancoward.dev>
2023-03-10 12:37:56 +0000

    Add ability to share

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import { FaDice } from "react-icons/fa";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useState } from "react";
+import { FaDice, FaLink } from "react-icons/fa";
 
 const imageArray = [
   "/hangman/0.svg",
@@ -24,6 +26,71 @@ const Hangman: NextPage = () => {
   const [gameOver, setGameOver] = useState(false);
   const [active, setActive] = useState(false);
   const [validWord, setValidWord] = useState(false);
+  const [copied, setCopied] = useState(false);
+
+  const router = useRouter();
+  const { w, l } = router.query;
+
+  const encodeString = (str: string) => {
+    return str
+      .split("")
+      .map((val) => val.charCodeAt(0).toString(16))
+      .join("");
+  };
+
+  const decodeString = (str: string) => {
+    let dec = "";
+    for (let i = 0; i < str.length; i += 2) {
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      dec += String.fromCharCode(parseInt(`${str[i]}${str[i + 1]}`, 16));
+    }
+    return dec;
+  };
+
+  const checkWord = useCallback(
+    (w: string) => {
+      if (!w) {
+        return false;
+      } else if (w.length < 3) {
+        return false;
+      } else if (!word.match(/^[a-zA-Z ]+$/)) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    [word]
+  );
+
+  useEffect(() => {
+    if (w && typeof w === "string") {
+      const decoded = decodeString(w);
+      setLetters([]);
+      setGuesses(0);
+      setGameOver(false);
+      setActive(true);
+      setWord(decoded);
+    }
+  }, [w]);
+
+  useEffect(() => {
+    if (l && typeof l === "string") {
+      const ls = l.split("");
+      setLetters(ls);
+      const guesses = ls.filter(
+        (letter) => letter !== " " && !word.includes(letter)
+      ).length;
+      setGuesses(guesses);
+    }
+  }, [l, word]);
+
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    }
+  }, [copied]);
 
   // get a random word
   const getRandomWord = async () => {
@@ -35,16 +102,8 @@ const Hangman: NextPage = () => {
   };
 
   useEffect(() => {
-    if (!word) {
-      setValidWord(false);
-    } else if (word.length < 3) {
-      setValidWord(false);
-    } else if (!word.match(/^[a-zA-Z ]+$/)) {
-      setValidWord(false);
-    } else {
-      setValidWord(true);
-    }
-  }, [word]);
+    setValidWord(checkWord(word));
+  }, [word, checkWord]);
 
   useEffect(() => {
     if (guesses >= 6) {
@@ -134,6 +193,7 @@ const Hangman: NextPage = () => {
                   </button>
                 ))}
               </div>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
                 src={getImage(guesses)}
                 alt="Hangman"
@@ -159,12 +219,31 @@ const Hangman: NextPage = () => {
                   setGuesses(0);
                   setGameOver(false);
                   setActive(false);
+                  router.push("/").catch((e) => console.log(e));
                 }}
               >
                 Reset Game
               </button>
             </p>
-            {/* if game over, show a modal saying win or loss */}
+            <p className="mt-4">
+              <button
+                onClick={() => {
+                  const link = `${window.location.origin}?w=${encodeString(
+                    word
+                  )}&l=${letters.length > 0 ? letters.join("") : ""}`;
+                  navigator.clipboard
+                    .writeText(link)
+                    .catch((err) => console.log(err));
+                  setCopied(true);
+                }}
+                className={`flex flex-row items-center gap-4 bg-blue-600 p-4 text-white ${
+                  copied ? "opacity-50" : ""
+                }`}
+              >
+                <FaLink className="text-2xl" />
+                <p className="text-lg">{copied ? "Copied" : "Share"}</p>
+              </button>
+            </p>
             {gameOver && (
               <div className="fixed top-0 left-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50">
                 <div className="rounded-md bg-white p-8">
@@ -243,7 +322,7 @@ const Hangman: NextPage = () => {
                 </button>
               </div>
 
-              <p>
+              <p className="text-2xl">
                 <button type="submit">Start</button>
               </p>
             </form>
